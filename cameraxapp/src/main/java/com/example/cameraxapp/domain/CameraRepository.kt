ec829d81f81478ed8a9c46d5c6a13df3c8c221bb
ttll <ttll442003@gmail.com>
2025-05-14 16:43:00 +0700

    Remove CameraXApp module: - Delete CameraXApp module and its related files. - Remove references to the module in `settings.gradle.kts`. - Remove the module entry in `.idea/gradle.xml`. - Update CameraX dependencies to the latest versions in `libs.versions.toml`. - Update lifecycle-runtime-ktx, compose-bom, material3, and navigation-compose dependencies to the latest versions in `libs.versions.toml`. - Delete mipmap images.

--- cameraxapp/src/main/java/com/example/cameraxapp/domain/CameraRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.cameraxapp.domain
-
-import android.net.Uri
-import androidx.camera.view.LifecycleCameraController
-import kotlinx.coroutines.flow.Flow
-
-interface CameraRepository {
-   suspend fun takePhoto(cameraController: LifecycleCameraController): Flow<Uri?>
-}
